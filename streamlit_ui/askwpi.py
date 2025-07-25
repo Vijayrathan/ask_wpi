ab3faac6c8088fb4357b52ae766981e0f995d8f7
Jeff Small <jsmall@Jeffs-Mac-mini.local>
2025-07-25 17:13:14 -0400

    Add first version of UI.

---
+++ streamlit_ui/askwpi.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, '..'))
+
+from react import react
+
+load_dotenv()
+
+query = ""
+
+st.title("Ask:red[WPI]")
+
+st.subheader("Chatbot Model")
+model = st.pills("", ["RAG", "ReACT", "Fine-Tuned LLM"], default="RAG")
+
+st.subheader("Question")
+query = st.text_area("")
+
+if query:
+    if model == "ReACT":
+        answer = react.run_react(query)
+    elif model == "RAG":
+        answer = "RAG is not implemented in the UI yet."
+    else:
+        answer = "Fine-Tuned LLM is not implemented in the UI yet."
+
+    st.subheader("Answer")
+    st.markdown(answer)
+
